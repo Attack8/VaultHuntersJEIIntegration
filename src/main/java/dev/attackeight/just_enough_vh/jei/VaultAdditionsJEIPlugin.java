99893b9058fabb6a025a171383154972fcde6139
radimous <jurda92@gmail.com>
2025-07-28 17:24:12 +0200

    make vaultadditions optional

--- src/main/java/dev/attackeight/just_enough_vh/jei/VaultAdditionsJEIPlugin.java
+++ src/main/java/dev/attackeight/just_enough_vh/jei/VaultAdditionsJEIPlugin.java
@@ -1,6 +1,9 @@
 package dev.attackeight.just_enough_vh.jei;
 
 import io.github.a1qs.vaultadditions.config.Configs;
+import io.github.a1qs.vaultadditions.init.ModBlocks;
+import mezz.jei.api.helpers.IGuiHelper;
+import mezz.jei.api.registration.IRecipeCategoryRegistration;
 import mezz.jei.api.registration.IRecipeRegistration;
 
 import static dev.attackeight.just_enough_vh.jei.JEIRecipeProvider.getStatueLoot;
@@ -13,5 +16,12 @@ public class VaultAdditionsJEIPlugin {
 		registration.addRecipes(VA_GIFT_STATUE, getStatueLoot(Configs.STATUE_LOOT_GIFT));
 		registration.addRecipes(VA_MEGA_GIFT_STATUE, getStatueLoot(Configs.STATUE_LOOT_MEGA_GIFT));
 		registration.addRecipes(VA_VAULT_STATUE, getStatueLoot(Configs.STATUE_LOOT_VAULT));
+	}
+
+	public static void registerStatueCategories(IRecipeCategoryRegistration registration, IGuiHelper guiHelper) {
+		registration.addRecipeCategories(makeLabeledLootInfoCategory(guiHelper, VA_ARENA_STATUE, ModBlocks.LOOT_STATUE_ARENA.get()));
+		registration.addRecipeCategories(makeLabeledLootInfoCategory(guiHelper, VA_GIFT_STATUE, ModBlocks.LOOT_STATUE_GIFT.get()));
+		registration.addRecipeCategories(makeLabeledLootInfoCategory(guiHelper, VA_MEGA_GIFT_STATUE, ModBlocks.LOOT_STATUE_GIFT_MEGA.get()));
+		registration.addRecipeCategories(makeLabeledLootInfoCategory(guiHelper, VA_VAULT_STATUE, ModBlocks.LOOT_STATUE_VAULT.get()));
 	}
 }
