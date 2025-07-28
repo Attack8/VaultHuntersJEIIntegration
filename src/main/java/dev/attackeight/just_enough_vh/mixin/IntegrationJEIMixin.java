b151e7d996f081507c38f6e2bc89123ba1067cba
attack8 <attackeight@gmail.com>
2025-07-28 15:02:21 -0400

    Modifications Cleanup deps Remove recipes too Fix mod box loot

--- src/main/java/dev/attackeight/just_enough_vh/mixin/IntegrationJEIMixin.java
+++ src/main/java/dev/attackeight/just_enough_vh/mixin/IntegrationJEIMixin.java
@@ -1,13 +1,19 @@
 package dev.attackeight.just_enough_vh.mixin;
 
 import iskallia.vault.integration.jei.IntegrationJEI;
+import iskallia.vault.integration.jei.lootbox.WeightedListJEI;
 import iskallia.vault.integration.jei.lootbox.WeightedListJEICategory;
+import iskallia.vault.integration.jei.materialbox.MaterialBoxJEI;
 import iskallia.vault.integration.jei.materialbox.MaterialBoxJEICategory;
+import mezz.jei.api.recipe.RecipeType;
 import mezz.jei.api.recipe.category.IRecipeCategory;
 import mezz.jei.api.registration.IRecipeCategoryRegistration;
+import mezz.jei.api.registration.IRecipeRegistration;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
+
+import java.util.List;
 
 @Mixin(value = IntegrationJEI.class, remap = false)
 public class IntegrationJEIMixin {
@@ -17,5 +23,12 @@ public class IntegrationJEIMixin {
         if (iRecipeCategories[0] instanceof WeightedListJEICategory || iRecipeCategories[0] instanceof MaterialBoxJEICategory)
             return;
         instance.addRecipeCategories(iRecipeCategories);
+    }
+
+    @Redirect(method = "registerRecipes", at = @At(value = "INVOKE", target = "Lmezz/jei/api/registration/IRecipeRegistration;addRecipes(Lmezz/jei/api/recipe/RecipeType;Ljava/util/List;)V"))
+    private <T> void removeRecipes(IRecipeRegistration instance, RecipeType<T> tRecipeType, List<T> ts) {
+        if (tRecipeType.getRecipeClass().equals(WeightedListJEI.class) || tRecipeType.getRecipeClass().equals(MaterialBoxJEI.class))
+            return;
+        instance.addRecipes(tRecipeType, ts);
     }
 }
